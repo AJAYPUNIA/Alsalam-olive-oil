eceff35196e86873628af34d1d5ab88a6b67dee2
AJAY PUNIA <punia09111508@gmail.com>
2025-06-14 14:15:49 -0400

    Fix Render deployment: bind to 0.0.0.0 and use dynamic port

--- app.py
+++ app.py
@@ -18,6 +18,9 @@ def services():
 def contact():
     return render_template('contact.html')
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
 
